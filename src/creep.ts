1895b7ab2779814c780e30dc11df6c37307a7c48
Adirelle <adirelle+github@gmail.com>
2017-09-16 16:38:04 +0200

    CS.

--- src/creep.ts
+++ src/creep.ts
@@ -4,7 +4,7 @@
  */
 interface Creep extends RoomObject, HitPoints {
     readonly prototype: Creep;
-    
+
     /**
      * An array describing the creep’s body. Each element contains the following properties:
      * type: string
@@ -57,7 +57,7 @@ interface Creep extends RoomObject, HitPoints {
     spawning: boolean;
     /**
      * The text message that the creep was saying at the last tick.
-     */ 
+     */
     saying: string;
     /**
      * The remaining amount of game ticks after which the creep will die.
@@ -188,7 +188,7 @@ interface Creep extends RoomObject, HitPoints {
      */
     say(message: string, toPublic?: boolean): ResultCode;
     /**
-     * Sign a controller with a random text visible to all players. This text will appear in the room UI, in the world map, and can be accessed via the API. 
+     * Sign a controller with a random text visible to all players. This text will appear in the room UI, in the world map, and can be accessed via the API.
      * You can sign unowned and hostile controllers. The target has to be at adjacent square to the creep. Pass an empty string to remove the sign.
      * @param target The target controller object to be signed.
      * @param text The sign text. The maximum text length is 100 characters.
