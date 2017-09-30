f3bf2cfef98ac79fe7a2dc4d4a9569356cfb457b
Adirelle <adirelle+github@gmail.com>
2017-09-30 20:59:00 +0200

    Updated FindPathOpts according to the code.

--- src/helpers.ts
+++ src/helpers.ts
@@ -80,12 +80,11 @@ interface LookAtResult {
     resource?: Resource;
 }
 
-
 interface LookAtResultMatrix {
     [coord: number]: LookAtResultMatrix|LookAtResult[]
 }
 
-interface FindPathOpts {
+interface CommonFindPathOpts {
   /**
    * Treat squares with creeps as walkable. Can be useful with too many moving creeps around or in some other cases. The default value is false.
    */
@@ -97,24 +96,9 @@ interface FindPathOpts {
   ignoreDestructibleStructures?: boolean;
 
   /**
-   * Ignore road structures. Enabling this option can speed up the search. The default value is false. This is only used when the new PathFinder is enabled.
+   * If true, the result path will be serialized using Room.serializePath. The default is false.
    */
-  ignoreRoads?: boolean;
-
-  /**
-   * You can use this callback to modify a CostMatrix for any room during the search. The callback accepts two arguments, roomName and costMatrix. Use the costMatrix instance to make changes to the positions costs. If you return a new matrix from this callback, it will be used instead of the built-in cached one. This option is only used when the new PathFinder is enabled.
-   */
-  costCallback?: (roomName: string, costMatrix: CostMatrix) => CostMatrix;
-
-  /**
-   * An array of the room's objects or RoomPosition objects which should be treated as walkable tiles during the search. This option cannot be used when the new PathFinder is enabled (use costCallback option instead).
-   */
-  ignore?: Array<RoomPosition | RoomObject>;
-
-  /**
-   * An array of the room's objects or RoomPosition objects which should be treated as obstacles during the search. This option cannot be used when the new PathFinder is enabled (use costCallback option instead).
-   */
-  avoid?: Array<RoomPosition | RoomObject>;
+  serialize?: boolean;
 
   /**
    * The maximum limit of possible pathfinding operations. You can limit CPU time used for the search based on ratio 1 op ~ 0.001 CPU. The default value is 2000.
@@ -125,19 +109,45 @@ interface FindPathOpts {
    * Weight to apply to the heuristic in the A formula F = G + weight H. Use this option only if you understand the underlying A* algorithm mechanics! The default value is 1.2.
    */
   heuristicWeight?: number;
-
-  /**
-   * If true, the result path will be serialized using Room.serializePath. The default is false.
-   */
-  serialize?: boolean;
-
-  /**
-   * The maximum allowed rooms to search. The default (and maximum) is 16. This is only used when the new PathFinder is enabled.
-   */
-  maxRooms?: number;
 }
 
-interface MoveToOpts extends FindPathOpts {
+interface NewFindPathOpts extends CommonFindPathOpts {
+  /**
+   * Ignore road structures. Enabling this option can speed up the search. The default value is false.
+   */
+  ignoreRoads?: boolean;
+
+  /**
+   * You can use this callback to modify a CostMatrix for any room during the search. The callback accepts two arguments, roomName and costMatrix. Use the costMatrix instance to make changes to the positions costs. If you return a new matrix from this callback, it will be used instead of the built-in cached one.
+   */
+  costCallback?: (roomName: string, costMatrix: CostMatrix) => CostMatrix;
+
+  /**
+   * The maximum allowed rooms to search. The default (and maximum) is 16.
+   */
+  maxRooms?: number;
+
+  /**
+   * The maximum range for which the goal is considered reached. The default is 0.
+   */
+  range?: number;
+}
+
+interface OldFindPathOpts extends CommonFindPathOpts {
+  /**
+   * An array of the room's objects or RoomPosition objects which should be treated as walkable tiles during the search. This option cannot be used when the new PathFinder is enabled (use costCallback option instead).
+   */
+  ignore?: Array<RoomPosition | RoomObject>;
+
+  /**
+   * An array of the room's objects or RoomPosition objects which should be treated as obstacles during the search. This option cannot be used when the new PathFinder is enabled (use costCallback option instead).
+   */
+  avoid?: Array<RoomPosition | RoomObject>;
+}
+
+type FindPathOpts = NewFindPathOpts | OldFindPathOpts;
+
+type MoveToOpts = FindPathOpts & {
     /**
      * This option enables reusing the path found along multiple game ticks. It allows to save CPU time, but can result in a slightly
      * slower creep reaction behavior. The path is stored into the creep's memory to the _move property. The reusePath value defines
